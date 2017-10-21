f6a68132208583f06510aac5939770a3091858f1
Yuki Fujiwara <sky.y.0079@gmail.com>
2017-10-21 23:47:06 +0900

    First commit.

---
+++ get_table.rb
@@ -0,0 +1,15 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+url = 'https://wiki.debian.org/DebianReleases'
+html = open(url).read
+doc = Nokogiri::HTML(html)
+
+# 下の行をコメントアウトすると、pryが起動する
+# binding.pry
+
+# 例：h2要素のみを抜き出す
+doc.css('h2').each do |node|
+  puts node.text
+end
